e71a8c57840ca1be8c0a6894ce1ed5c683e0ba21
Adryan Mendes <adryanmendes2014@gmail.com>
2023-03-25 15:22:41 -0300

    fix: consertando incompatibilidade
    consertando incompatibilidade no App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import './App.css';
 import Layout from './components/layout/layout/Layout';
 import Header from './components/layout/header/Header';
 import Footer from './components/layout/footer/Footer';
-<<<<<<< HEAD
 import PostList from './components/PostList/PostList';
 import PostDetails from './components/PostDetails/PostDetails';
-=======
-import PostList from './components/layout/PostList/PostList';
->>>>>>> 29250b4e6b674ab012c5e1ff2f0fee32c6330f73
 
 
 function App() {
@@ -16,14 +12,10 @@ function App() {
     <Layout>
       <Router>
         <Header />
-<<<<<<< HEAD
         <Routes>
           <Route path="/" element={<PostList />} />
           <Route path="/post/:id" element={<PostDetails />} />
         </Routes>
-=======
-          <PostList />
->>>>>>> 29250b4e6b674ab012c5e1ff2f0fee32c6330f73
         <Footer />
       </Router>
     </Layout>
